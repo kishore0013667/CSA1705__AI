c834bce99d7e1c3f21ae22ade9213779d56aac27
kishore0013667 <142767995+kishore0013667@users.noreply.github.com>
2023-12-01 11:52:21 +0530

    Add files via upload

---
+++ 26)fruit and its colour back tracking.pl
@@ -0,0 +1,11 @@
+fruit_color(apple, red).
+fruit_color(banana, yellow).
+fruit_color(grape, purple).
+fruit_color(orange, orange).
+fruit_color(plum, purple).
+fruit_color(strawberry, red).
+fruit_color(blueberry, blue).
+
+get_fruit_color(Fruit, Color) :- fruit_color(Fruit, Color).
+
+fruits_of_color(Color, Fruits) :- findall(Fruit, fruit_color(Fruit, Color), Fruits).
